20c863bcc59d43670dd18242bee7777da50fad5d
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-03-24 23:56:12 +0900

    1주차 과제 함수 생성

---
+++ week1.py
@@ -0,0 +1,21 @@
+from numpy import *
+from numpy import array, arange
+
+Arr1=array([arange(1,2),arange(3,4),arange(5,6)])
+Arr2=array([[2,3,5],[1,7,8],[-1,-2,-3]])
+
+issame=(Arr1 is Arr2)
+print(issame)
+#같은지
+
+add=Arr1 + Arr2
+print(add)
+#덧셈
+
+mul=Arr1 * Arr2
+print(mul)
+#곱셈
+
+sub=Arr1-Arr2
+print(sub)
+#뺼셈
