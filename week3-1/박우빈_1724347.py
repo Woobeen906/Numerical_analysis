07e0ee9dd13223a6a3b93427d010b5849e624c89
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-03-28 20:23:10 +0900

    3주차 첫번째 과제

---
+++ week3-1/박우빈_1724347.py
@@ -0,0 +1,12 @@
+from numpy import linspace,cos,sin,pi
+import matplotlib.pyplot as plt
+x = linspace(0,2.0*pi,40)
+yc = cos(x)
+ys = sin(x)
+plt.plot(x,yc,'ro-',label='cos(x)')
+plt.plot(x,ys,'d--',label='sin(x)')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('y=cos(x) & sin(x)')
+plt.show()
